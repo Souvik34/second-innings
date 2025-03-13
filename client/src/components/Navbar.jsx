ca3746e76744e781accf6c9ad27b1452f1195f2e
Aditya <adityasahoo652@gmail.com>
2025-03-13 09:46:08 +0530

    Implement theme toggle functionality in Navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,16 +3,20 @@ import { Link } from "react-router-dom";
 import "./styles/navbar.css";
 import "./styles/darkmode.css";
 import navpic from "./images/logo.png";
+import { useTheme } from "../context/ThemeContext";
+import { BsSunFill, BsMoonFill } from "react-icons/bs"
 // import { useState } from "react";
 
 export default function Navbar() {
+  const { theme, toggleTheme } = useTheme();
+
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-gradient bg-light rounded">
+      <nav className={`navbar navbar-expand-lg bg-gradient ${theme === "light" ? "bg-light navbar-light" : "bg-dark"} rounded`}>
         <div className="container-fluid">
           <img className="mx-1" src={navpic} alt="" height="60" width="60" />
           <Link className="navbar-brand col-md-1 mx-1 text-center" to="/">
-            <p className="navMainTxt mt-3 fw-bold fs-3">
+            <p className={`navMainTxt mt-3 fw-bold fs-3 ${theme === "light" ? "text-dark" : "text-light"}`}>
               SECOND INNINGS
               {/* <p style={{ fontSize: "12px" }}>The Coding Club of UIT, BU</p> */}
             </p>
@@ -36,44 +40,49 @@ export default function Navbar() {
             <nav  className="navbar navbar-expand-lg bg-body-tertiary">
               <div  className="container-fluid  ">
                 <div  className="collapse navbar-collapse  " id="navbarNavDropdown">
-                  <ul  className="navbar-nav  fs-5">
+                  <ul  className="navbar-nav  fs-5 align-items-center">
                     <li  className="nav-item">
                       <Link className="nav-link" to="/">
-                        <p className="text-center fw-bold st">Home</p>
+                        <p className={`text-center fw-bold st mb-0 ${theme === "light" ? "text-dark" : "text-white"}`}>Home</p>
                       </Link>
                     </li>
                     <li className="nav-item">
                       <Link className="nav-link" to="/adopt">
-                        <p className="text-center fw-bold st">Adopt parents</p>
+                        <p className={`text-center fw-bold st mb-0 ${theme === "light" ? "text-dark" : "text-white"}`}>Adopt parents</p>
                       </Link>
                     </li>
                     <li className="nav-item">
                       <Link className="nav-link" to="/gethome">
-                        <p className="text-center fw-bold st">Find a home</p>
+                        <p className={`text-center fw-bold st mb-0 ${theme === "light" ? "text-dark" : "text-white"}`}>Find a home</p>
                       </Link>
                     </li>
                     
 
                     <li className="nav-item">
                       <Link className="nav-link" to="/gallery">
-                        <p className="text-center fw-bold st"> Gallery</p>
+                        <p className={`text-center fw-bold st mb-0 ${theme === "light" ? "text-dark" : "text-white"}`}> Gallery</p>
                       </Link>
                     </li>
 
                     <li className="nav-item">
                       <Link className="nav-link" to="/contact">
-                        <p className="text-center fw-bold st">Contact</p>
+                        <p className={`text-center fw-bold st mb-0 ${theme === "light" ? "text-dark" : "text-white"}`}>Contact</p>
                       </Link>
                     </li>
                     <li className="nav-item">
                       <Link className="nav-link" to="/faq">
-                        <p className="text-center fw-bold st">FAQs</p>
+                        <p className={`text-center fw-bold st mb-0 ${theme === "light" ? "text-dark" : "text-white"}`}>FAQs</p>
                       </Link>
                     </li>
                     <li className="nav-item">
                       <Link className="nav-link" to="/signin">
-                        <p className="text-center fw-bold st">Login</p>
+                        <p className={`text-center fw-bold st mb-0 ${theme === "light" ? "text-dark" : "text-white"}`}>Login</p>
                       </Link>
+                    </li>
+                     <li className="nav-item">
+                        <button className={`btn p-1 border-0 outline-0 shadow-none bg-transparent transition ease-out ${theme === "light" ? "text-dark" : "text-white"}`} onClick={toggleTheme}>
+                          {theme === "light" ? <BsMoonFill size={25} /> : <BsSunFill size={25} />}
+                        </button>
                     </li>
                   </ul>
                 </div>
